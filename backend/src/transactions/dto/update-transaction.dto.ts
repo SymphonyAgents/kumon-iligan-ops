9a6961becc9b37d7b8fdee8e3626b72811d2b5d1
gleepgorp <staphmaster2002@gmail.com>
2026-02-25 20:05:02 +0800

    [Feat][Vince] Add note field to UpdateTransactionDto

--- backend/src/transactions/dto/update-transaction.dto.ts
+++ backend/src/transactions/dto/update-transaction.dto.ts
@@ -3,6 +3,7 @@ export class UpdateTransactionDto {
   customerPhone?: string;
   customerEmail?: string;
   status?: string;
+  note?: string;
   pickupDate?: string;
   total?: string;
   paid?: string;
